e97f8cc63f25dab0f74babb8bda5b16d73cec838
leewonz <lwk530@gmail.com>
2020-08-21 17:37:29 +0900

    change name

--- src/main/java/elecshopping/CustomerVO.java
+++ src/main/java/elecshopping/CustomerVO.java
@@ -1,5 +1,41 @@
 package elecshopping;
 
 public class CustomerVO {
-
+	String id;
+	String pw;
+	String name;
+	String phone;
+	int admin;
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getPw() {
+		return pw;
+	}
+	public void setPw(String pw) {
+		this.pw = pw;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	public int getAdmin() {
+		return admin;
+	}
+	public void setAdmin(int admin) {
+		this.admin = admin;
+	}
+	
+	
 }
