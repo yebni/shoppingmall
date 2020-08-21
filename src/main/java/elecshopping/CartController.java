4e14123576d2f3a5fcc7a2c269cf8cdfbca9f3cb
leewonz <lwk530@gmail.com>
2020-08-21 17:49:05 +0900

    cart.jsp와 CartController.java 매핑

--- src/main/java/elecshopping/CartController.java
+++ src/main/java/elecshopping/CartController.java
@@ -1,5 +1,12 @@
 package elecshopping;
 
-public class CartController {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
+public class CartController {
+	@RequestMapping("/cart") // URL "/shop/cart" 로 매핑함
+	public String cart() {
+		return "cart"; //cart.jsp 보여줌
+	}
 }
